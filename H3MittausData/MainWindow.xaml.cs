a9661e6e3aa46fe72daf847499dde58b77312048
EsaSalmik <esa.salmikangas@jamk.fi>
2016-02-03 11:23:13 +0200

    Create functionality for writing/reading text files

--- H3MittausData/MainWindow.xaml.cs
+++ H3MittausData/MainWindow.xaml.cs
@@ -21,6 +21,8 @@ namespace H3MittausData
   /// </summary>
   public partial class MainWindow : Window
   {
+    //luodaan kokoelma mittaus-olioille
+    List<MittausData> mitatut;
     public MainWindow()
     {
       InitializeComponent();
@@ -30,13 +32,60 @@ namespace H3MittausData
     {
       //omat ikkunaan liittyvät alustukset
       txtToday.Text = DateTime.Today.ToShortDateString();
+      mitatut = new List<MittausData>();
     }
 
     private void btnSaveData_Click(object sender, RoutedEventArgs e)
     {
       //luodaan uusi mittausdata olio ja näytetään se käyttäjälle
       MittausData md = new MittausData(txtClock.Text, txtData.Text);
-      lbData.Items.Add(md);
+      //lbData.Items.Add(md);//alkuperäinen tapa 
+      //lisätään mittaus-olio kokoelmaan
+      mitatut.Add(md);
+      ApplyChanges();
+    }
+    private void ApplyChanges()
+    {
+      //päivitetään UI vastaamaan kokoelman tietoja
+      lbData.ItemsSource = null;
+      lbData.ItemsSource = mitatut;
+    }
+
+    private void btnSaveToFile_Click(object sender, RoutedEventArgs e)
+    {
+      // kutsu BL:n tallennusmetodia
+      try
+      {
+        MittausData.SaveDataToFile(mitatut, txtFileName.Text);
+        MessageBox.Show("Tiedot tallennettu onnistuneesti tiedostoon " + txtFileName.Text);
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
+    }
+
+    private void btGetFromFile_Click(object sender, RoutedEventArgs e)
+    {
+      //luetaan datat käyttäjän antamasta tiedostosta
+      try
+      {
+        mitatut = null;
+        mitatut = MittausData.ReadDataFromFile(txtFileName.Text);
+        ApplyChanges();
+        MessageBox.Show("Tiedot luettu onnistuneesti tiedostosta " + txtFileName.Text);
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
+
+    }
+
+    private void btnSaveToXML_Click(object sender, RoutedEventArgs e)
+    {
+      //serialisoidaan XML:ksi
+      JAMK.IT.IIO11300.Serialisointi.SerialisoiXml(@"d:\testi.xml", mitatut);
     }
   }
 }
